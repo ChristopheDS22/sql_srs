98bec347ec8ad5a94344169a999daa84e214955b
ChristopheDS22 <113445317+ChristopheDS22@users.noreply.github.com>
2025-03-04 22:01:46 +0100

    Revert "premiere_modif"

--- app.py
+++ app.py
@@ -1,48 +1,34 @@
 import streamlit as st
 import pandas as pd
 import duckdb
-import io
 
-csv = '''
-beverage,price
-orange juice,2.5
-Expresso,2
-Tea,3
-'''
-beverages = pd.read_csv(io.StringIO(csv))
+st.write("""
+#SQL SRS - Speed Repetition System SQL pratice
+""")
 
-csv2 = '''
-food_item,food_price
-cookie juice,2.5
-chocolatine,2
-muffin,3
-'''
-food_items = pd.read_csv(io.StringIO(csv2))
+option = st.selectbox(
+    "Que voulez-vous réviser?",
+    ("Joins", "GroupBy", "Windows Functions"),
+    index=None,
+    placeholder="Sélectionner un thème..."
+)
 
-answer = """
-SELECT * FROM beverages
-CROSS JOIN food_items
-"""
+st.write("Vous avez sélectionné:", option)
 
-solution = duckdb.sql(answer).df()
+data = {
+    "a" : [1,2,3],
+    "b" : [4,5,6]
+}
 
+df = pd.DataFrame(data)
 
-st.header("enter your code:")
-query = st.text_area(label="votre code SQL ici", key="user_input")
-if query:
-    result = duckdb.sql(query).df()
-    st.dataframe(result)
+st.write("df:")
+st.dataframe(df)
 
-
-tab2, tab3 = st.tabs(["Tables", "Solution"])
-
-with tab2:
-    st.write("table: beverages")
-    st.dataframe(beverages)
-    st.write("table: food_items")
-    st.dataframe(food_items)
-    st.write("expected:")
-    st.dataframe(solution)
-
-with tab3:
-    st.write(answer)
+code = st.text_area(
+    "Ecrivez votre code ici:",
+    placeholder="Saisissez votre code..."
+)
+resultat = duckdb.query(code).df()
+st.write(f"votre résultat: {code}")
+st.dataframe(resultat)
