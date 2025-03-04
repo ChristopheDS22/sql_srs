1e5ee1a8ee6c13dba6909edbad7996d99810cd4a
Christophe <christophe@PC-Christophe>
2025-03-04 22:10:07 +0100

    Revert "Merge pull request #2 from ChristopheDS22/add_theme_selection"
    This reverts commit 689a298b6a0233006c67dc804182bc24e46f7583, reversing
    changes made to b2fda792ac5a339444cc3df0110f2c9fb6c4b614.

--- app.py
+++ app.py
@@ -2,18 +2,7 @@ import streamlit as st
 import pandas as pd
 import duckdb
 
-st.write("""
-#SQL SRS - Speed Repetition System SQL pratice
-""")
-
-option = st.selectbox(
-    "Que voulez-vous réviser?",
-    ("Joins", "GroupBy", "Windows Functions"),
-    index=None,
-    placeholder="Sélectionner un thème..."
-)
-
-st.write("Vous avez sélectionné:", option)
+st.write("Première app")
 
 data = {
     "a" : [1,2,3],
@@ -25,10 +14,7 @@ df = pd.DataFrame(data)
 st.write("df:")
 st.dataframe(df)
 
-code = st.text_area(
-    "Ecrivez votre code ici:",
-    placeholder="Saisissez votre code..."
-)
+code = st.text_area("Ecrivez votre code ici:")
 resultat = duckdb.query(code).df()
 st.write(f"votre résultat: {code}")
 st.dataframe(resultat)
