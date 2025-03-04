ffe38658278fb8aaab7d34eb2610538873bae3f5
Christophe <christophe@PC-Christophe>
2025-03-04 14:03:25 +0100

    add_theme_selection

--- app.py
+++ app.py
@@ -2,7 +2,18 @@ import streamlit as st
 import pandas as pd
 import duckdb
 
-st.write("Première app")
+st.write("""
+#SQL SRS - Speed Repetition System SQL pratice
+""")
+
+option = st.selectbox(
+    "Que voulez-vous réviser?",
+    ("Joins", "GroupBy", "Windows Functions"),
+    index=None,
+    placeholder="Sélectionner un thème..."
+)
+
+st.write("Vous avez sélectionné:", option)
 
 data = {
     "a" : [1,2,3],
@@ -14,7 +25,10 @@ df = pd.DataFrame(data)
 st.write("df:")
 st.dataframe(df)
 
-code = st.text_area("Ecrivez votre code ici:")
+code = st.text_area(
+    "Ecrivez votre code ici:",
+    placeholder="Saisissez votre code..."
+)
 resultat = duckdb.query(code).df()
 st.write(f"votre résultat: {code}")
 st.dataframe(resultat)
